805373050c6b4d20dd10eaff1f7a7758206da132
Daniel Król <danielkrol90@gmail.com>
2022-07-07 23:23:02 +0200

    made working api for oldPosts,  now button for it

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -3,34 +3,52 @@ import axios from "axios";
 import "./HomePage.css";
 import Post from "../components/Posts";
 
-
 const HomePage = (props) => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [oldPosts, setOldPosts] = useState([]);
+
+
+  useEffect(() => {
+    getLatestPosts();
+    getOlderPosts();
+  }, []);
   
-    useEffect(() => {
-      getLatestPosts();
-    }, []);
+
   
-    const getLatestPosts = () => {
-      axios
-        .post("https://akademia108.pl/api/social-app/post/latest")
-        .then((res) => {
-          setPosts(res.data);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log("AXIOS ERROR: ", err);
-        });
-    };
-  
-    return (
-   
-        <div className="postBoard">
-            {posts.map((post) => { return <Post data={post} key={post.id} />})}
-            <button className="postBtn">Load more</button>
-        </div>
-     
-    );
+
+  const getLatestPosts = () => {
+    axios
+      .post("https://akademia108.pl/api/social-app/post/latest")
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log("AXIOS ERROR: ", err);
+      });
   };
-  
-  export default HomePage;
+
+  const getOlderPosts = () => {
+    axios
+      .post("https://akademia108.pl/api/social-app/post/older-then", {"date": "2020-05-05T07:02:14.000000Z"})
+
+      .then((res) => 
+       setOldPosts(res.data))
+
+      .catch((err) => {
+        console.log("AXIOS ERROR: ", err)
+      })
+  };
+
+  return (
+    <div className="container">
+      <div className="postBoard">
+        {posts.map((post) => {
+          return <Post data={post} key={post.id} />
+        })}
+        <button className="postBtn"onClick={Post}>Load more </button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
