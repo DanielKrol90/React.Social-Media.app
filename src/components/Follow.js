82421bd5f416fdf0b0210c3d3320c519b18e534d
Daniel Król <danielkrol90@gmail.com>
2022-07-19 16:15:28 +0200

    progress on rec/follow list

--- src/components/Follow.js
+++ src/components/Follow.js
@@ -1,25 +1,23 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Follow = (props) => {
-
   useEffect(() => {
     return () => {
       followRecommendations();
       followAllData();
     };
-  }, [])
-
+  }, []);
 
   const [followList, setFollowList] = useState([]);
   const [followAll, setFollowAll] = useState([]);
 
   const followRecommendations = () => {
     axios
-      .post("https://akademia108.pl/api/social-app/follows/recommendations",)
+      .post("https://akademia108.pl/api/social-app/follows/recommendations")
       .then((res) => {
         setFollowList(res.data);
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch((err) => {
         console.log("AXIOS ERROR: ", err);
@@ -31,7 +29,6 @@ const Follow = (props) => {
       .post("https://akademia108.pl/api/social-app/follows/allfollows")
       .then((res) => {
         setFollowAll(res.data);
-   
       })
       .catch((err) => {
         console.log(err);
@@ -41,24 +38,23 @@ const Follow = (props) => {
   const addFollow = () => {
     axios
       .post("https://akademia108.pl/api/social-app/follows/follow", {
-        leader_id:  props.user.id
+        leader_id: props.user.id,
       })
 
       .then((res) => {
         followAllData(res.data);
-        followRecommendations(res.data)
-        console.log(res.data)
+        followRecommendations(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-
   const deleteFollow = () => {
     axios
       .post("https://akademia108.pl/api/social-app/follows/disfollow", {
-        leader_id: props.user.id
+        leader_id: props.user.id,
       })
       .then((res) => {
         followAllData(res.data);
@@ -69,32 +65,42 @@ const Follow = (props) => {
   };
 
   let liFollowList = followList.map((user) => {
-
     return (
       <li key={user.id}>
         <img src={user.avatar_url} alt="avatar" />
         <span>{user.username}</span>
-        <button className="btnFollow" onClick={() => {addFollow(user.id);}}>
-        Add Follow
+        <button
+          className="btnFollow"
+          onClick={() => {
+            addFollow(user.id);
+          }}
+        >
+          Follow
         </button>
-        <button className="btnUnFollow" onClick={() => {deleteFollow(user.id);}}>
-          Delete Follow
+        <button
+          className="btnUnFollow"
+          onClick={() => {
+            deleteFollow(user.id);
+          }}
+        >
+          Unfollow
         </button>
       </li>
     );
   });
 
   let liFollowAll = followAll.map((user) => {
-
     return (
       <li key={user.id}>
         <img src={user.avatar_url} alt="avatar" />
         <span>{user.username}</span>
-        <button className="btnFollow" onClick={() => {addFollow(user.id);}}>
-        Add Follow
-        </button>
-        <button className="btnUnFollow" onClick={() => {deleteFollow(user.id);}}>
-          Delete Follow
+        <button
+          className="btnUnFollow"
+          onClick={() => {
+            deleteFollow(user.id);
+          }}
+        >
+          UnFollow
         </button>
       </li>
     );
@@ -102,10 +108,10 @@ const Follow = (props) => {
 
   return (
     <div className="followList">
-      <h2>Recomendation to Follow</h2>
-      <ul>{liFollowList}</ul>
-      <h2>You Follow Them</h2>
-      <ul>{liFollowAll}</ul>
+        <h2 className="RecListHeader">Recomendation to Follow</h2>
+        <ul className="rec-list">{liFollowList}</ul>
+        <h2 className="followListHeader">List of Followed</h2>
+        <ul className="follow-list">{liFollowAll}</ul>
     </div>
   );
 };
